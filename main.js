a925c25ae59dd2f9a37ee0bfc3375f7fb29e01dc
Aleximena2022 <113704057+Aleximena2022@users.noreply.github.com>
2023-10-25 00:21:19 -0300

    se crea proyecto para la  primera entrega Coder javascript Comision 49760

---
+++ main.js
@@ -0,0 +1,51 @@
+// Funciones
+function suma(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    return a - b;
+}
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b === 0) {
+        return "Operación incorrecta";
+    } else {
+        return a / b;
+    }
+}
+
+// Algoritmo condicional
+let a = Number(prompt("Ingresar un número"));
+let operacion = prompt("Operación Matemática");
+let b = Number(prompt("Ingresar un número"));
+
+switch (operacion) {
+    case '+':
+        console.log(suma(a, b));
+        break;
+    case '-':
+        console.log(resta(a, b));
+        break;
+    case '*':
+        console.log(multiplicacion(a, b));
+        break;
+    case '/':
+        console.log(division(a, b));
+        break;
+    default:
+        console.log("Operación incorrecta");
+}
+
+// Algoritmo generando un ciclo
+let numero = Number(prompt("Ingresar un número"));
+
+while (numero !== 0) {
+    console.log(numero);
+    numero = Number(prompt("Ingresar otro número"));
+}
+console.log("Fin De La Operacion");
